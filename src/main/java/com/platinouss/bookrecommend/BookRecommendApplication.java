0b66b30601bc0849ae1edabf2b434d5ae6331149
platinouss <platinouss@gmail.com>
2022-08-09 15:31:04 +0900

    Initial commit

---
+++ src/main/java/com/platinouss/bookrecommend/BookRecommendApplication.java
@@ -0,0 +1,13 @@
+package com.platinouss.bookrecommend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookRecommendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookRecommendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/platinouss/bookrecommend/BookRecommendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.platinouss.bookrecommend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookRecommendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
