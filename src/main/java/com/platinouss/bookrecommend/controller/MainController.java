ecc567b3327b176ab2d8ca40e55bd5cb9e90c04a
platinouss <platinouss@gmail.com>
2022-10-19 06:29:38 +0900

    기존 Controller 및 SecurityConfig 구성 수정

--- src/main/java/com/platinouss/bookrecommend/controller/MainController.java
+++ src/main/java/com/platinouss/bookrecommend/controller/MainController.java
@@ -1,26 +1,41 @@
 package com.platinouss.bookrecommend.controller;
 
-import com.platinouss.bookrecommend.service.BookService;
-import com.platinouss.bookrecommend.service.ReviewService;
+import com.platinouss.bookrecommend.domain.User;
+import com.platinouss.bookrecommend.service.UserService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Required;
 import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 @RequiredArgsConstructor
 public class MainController {
 
-    private final BookService bookService;
-    private final ReviewService reviewService;
+    private final UserService userService;
 
     @GetMapping("/")
-    public String main(Authentication authentication, Model model) {
-        model.addAttribute("username", authentication.getName());
-        model.addAttribute("books", bookService.getAll());
-        model.addAttribute("reviews", reviewService.getAll());
+    public String main(Authentication authentication, Model m) {
+        if(authentication != null) {
+            m.addAttribute("username", authentication.getName());
+        }
 
         return "main.html";
+    }
+
+    @GetMapping("/register")
+    public String register() {
+        return "register.html";
+    }
+
+    @PostMapping("/register")
+    public String register(User user) {
+        if(!userService.exists(user.getName())) {
+            userService.addUser(user);
+        }
+
+        return "redirect:/";
     }
 }
---
+++ src/main/java/com/platinouss/bookrecommend/controller/MyPageController.java
@@ -0,0 +1,26 @@
+package com.platinouss.bookrecommend.controller;
+
+import com.platinouss.bookrecommend.service.BookService;
+import com.platinouss.bookrecommend.service.ReviewService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.Authentication;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+@RequiredArgsConstructor
+public class MyPageController {
+
+    private final BookService bookService;
+    private final ReviewService reviewService;
+
+    @GetMapping("/mypage")
+    public String main(Authentication authentication, Model model) {
+        model.addAttribute("username", authentication.getName());
+        model.addAttribute("books", bookService.getAll());
+        model.addAttribute("reviews", reviewService.getAll());
+
+        return "mypage.html";
+    }
+}
