3bbd582763ebc4a3f52720cd9f41fc26fe7948f9
platinouss <platinouss@gmail.com>
2022-10-27 19:35:55 +0900

    test : 일부 테스트코드 수정

--- src/test/java/com/platinouss/bookrecommend/service/SearchBookServiceTest.java
+++ src/test/java/com/platinouss/bookrecommend/service/SearchBookServiceTest.java
@@ -3,11 +3,13 @@ package com.platinouss.bookrecommend.service;
 import com.platinouss.bookrecommend.dto.BookDto;
 import lombok.NoArgsConstructor;
 import lombok.RequiredArgsConstructor;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 import java.io.IOException;
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -18,12 +20,27 @@ class SearchBookServiceTest {
     private SearchBookService searchBookService;
 
     @Test
-    void searchBookServiceTest() throws IOException {
-        BookDto bookDto =  searchBookService.search("잘될 수밖에 없는 너에게");
-        System.out.println(bookDto);
+    @DisplayName("1. 정상적인 책 1권만 검색")
+    void tes1() throws IOException {
+        List<BookDto> bookDto =  searchBookService.search("잘될 수밖에 없는 너에게");
+        assertEquals(1, bookDto.size());
 
-        assertEquals(9791191891201L, bookDto.getIsbn());
-        assertEquals("시/에세이", bookDto.getCategory());
-        assertEquals("북로망스", bookDto.getPublisher());
+        assertEquals(9791191891201L, bookDto.get(0).getIsbn());
+        assertEquals("시/에세이", bookDto.get(0).getCategory());
+        assertEquals("북로망스", bookDto.get(0).getPublisher());
+    }
+
+    @Test
+    @DisplayName("2. 광범위한 이름으로 여러 권 검색")
+    void test2() throws IOException {
+        List<BookDto> bookDto =  searchBookService.search("철학");
+        assertEquals(10, bookDto.size());
+    }
+
+    @Test
+    @DisplayName("3. 비정상적인 이름으로 검색")
+    void test3() throws IOException {
+        List<BookDto> bookDto =  searchBookService.search("^~!");
+        assertEquals(0, bookDto.size());
     }
 }
--- src/test/java/com/platinouss/bookrecommend/support/MyCrawlerTest.java
+++ src/test/java/com/platinouss/bookrecommend/support/MyCrawlerTest.java
@@ -3,6 +3,7 @@ package com.platinouss.bookrecommend.support;
 import com.platinouss.bookrecommend.naver.NaverClient;
 import com.platinouss.bookrecommend.naver.dto.SearchBookReq;
 import com.platinouss.bookrecommend.naver.dto.SearchBookRes;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -18,15 +19,16 @@ class MyCrawlerTest {
     private NaverClient naverClient;
 
     @Test
+    @DisplayName("1. Book 카테고리 값 가져오기")
     void categoryCrawlerTest() throws IOException {
         SearchBookReq search = new SearchBookReq();
-        search.setQuery("운영체제");
+        search.setQuery("잘될 수밖에 없는 너에게");
 
         SearchBookRes result = naverClient.bookSearch(search);
 
         MyCrawler myCrawler = new MyCrawler();
         String category = myCrawler.categoryCrawler(result.getItems().get(0).getLink());
 
-        assertEquals(category, "컴퓨터/IT");
+        assertEquals(category, "시/에세이");
     }
 }
